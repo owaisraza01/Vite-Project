b08336a7fad64ce3ee14a0c97a7aed1f1cc735c0
Owaisraza <owaisharoon00@gmail.com>
2024-12-15 14:24:53 +0500

    First Commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById("root")).render(
+  <div>
+    <h1>My name is Muhamamd Owais Raza</h1>
+    <img src="https://stowfitnesscenter.com/wp-content/uploads/2019/05/check-back-soon.png" alt="" srcset="" style={{height: 600}} />
+  </div>
+)
